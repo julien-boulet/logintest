e1e89f23fafab1f3c82b323589f3b360519df065
julien-boulet <julien.boulet.dev@gmail.com>
2018-05-31 11:36:40 +0200

    use pool mysql connection

--- config/db.js
+++ config/db.js
@@ -1,11 +1,28 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit: 10,
   host: "localhost",
   user: "root",
   password: "root",
   database: "test"
 });
 
-connection.connect();
+connection = {
+  query: function() {
+    var queryArgs = Array.prototype.slice.call(arguments);
+
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        throw err;
+      }
+      if (connection) {
+        var q = connection.query.apply(connection, queryArgs);
+        q.on("end", function() {
+          connection.release();
+        });
+      }
+    });
+  }
+};
 
 module.exports = connection;
